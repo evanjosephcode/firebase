98ad3e4e12b299822a63d9dfd86ca86d26fee8f8
evanjosephcode <evanseaman89@gmail.com>
2018-03-11 20:55:48 -0500

    added filestructure and index for table/row

---
+++ public/trainlogic.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDP5XRPt6CjMpDFNDEmvIhuElLq5lhQkgs",
+        authDomain: "mwclass-seaman.firebaseapp.com",
+        databaseURL: "https://mwclass-seaman.firebaseio.com",
+        projectId: "mwclass-seaman",
+        storageBucket: "",
+        messagingSenderId: "78077600785"
+};
+
+firebase.initializeApp(config);
+
